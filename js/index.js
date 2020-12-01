02c19fb4e6a30283b3c814e375505a84408b953d
edvinhellsing <edvinhellsing@gmail.com>
2020-12-01 17:02:40 +0100

    update with for loop intstead of map

--- js/index.js
+++ js/index.js
@@ -4,38 +4,40 @@ import ReactDOM from 'react-dom';
 import InsultItem from './InsultItem';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-        this.state = {
-            insults: [
-                { insult: "Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?", play: "Macbeth" },
-                { insult: "Never hung poison on a fouler toad", play: "Richard III" },
-                { insult: "He thinks too much: such men are dangerous.", play: "Julius Ceasar" },
-                { insult: "Thou calledst me a dog before thou hadst a cause. But since I am a dog, beware my fangs.", play: "The Merchant of Venice" },
-                { insult: "Give me your hand...I can tell your fortune. You are a fool.", play: "The Two Noble Kinsmen" },
-                { insult: "He smells like a fish, a very ancient and fish-like smell, a kind of not-of-the-newest poor-John. A strange fish!", play: "The Tempest" },
-                { insult: "It is a tale Told by an idiot, full of sound and fury, Signifying nothing.", play: "Macbeth" },
-                { insult: "Alas, poor heart, that kiss is comfortless As frozen water to a starved snake.", play: "Titus Andronicus" },
-                { insult: "He hath eaten me out of house and home; he hath put all substance into that fat belly of his.", play: "Henry IV, Part 2" },
-                { insult: "Out, you green-sickness carrion! Out, you baggage! You tallow-face!", play: "Romeo and Juliet" }
-
-            ]
-        }
+   constructor(props) {
+      super(props);
+      console.log(props);
+      this.state = {
+         insults: [
+            { insult: "Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?", play: "Macbeth" },
+            { insult: "Never hung poison on a fouler toad", play: "Richard III" },
+            { insult: "He thinks too much: such men are dangerous.", play: "Julius Ceasar" },
+            { insult: "Thou calledst me a dog before thou hadst a cause. But since I am a dog, beware my fangs.", play: "The Merchant of Venice" },
+            { insult: "Give me your hand...I can tell your fortune. You are a fool.", play: "The Two Noble Kinsmen" },
+            { insult: "He smells like a fish, a very ancient and fish-like smell, a kind of not-of-the-newest poor-John. A strange fish!", play: "The Tempest" },
+            { insult: "It is a tale Told by an idiot, full of sound and fury, Signifying nothing.", play: "Macbeth" },
+            { insult: "Alas, poor heart, that kiss is comfortless As frozen water to a starved snake.", play: "Titus Andronicus" },
+            { insult: "He hath eaten me out of house and home; he hath put all substance into that fat belly of his.", play: "Henry IV, Part 2" },
+            { insult: "Out, you green-sickness carrion! Out, you baggage! You tallow-face!", play: "Romeo and Juliet" }
+         ]
+      }
     }
 
-    render() {
-        return (
+   render() {
+      let i = 0;
+      const items = [];
+      for (i = 0; i < 10; i++) {
+         items.push (<InsultItem insult = { this.state.insults[i].insult } play= { this.state.insults[i].play } />)
+      }
+         return (
             <article>
-                <h1>{this.props.title}</h1>
-                <ul className="insult-list">
-                    { this.state.insults.map((insult) => {
-                        return <InsultItem insult = { insult.insult } play= { insult.play } />
-                    }) }
-                </ul>
+               <h1>{this.props.title}</h1>
+               <ul className="insult-list">
+                  {items}
+               </ul>
             </article>
-        )
-    }
+         )
+   }
 }
 
 ReactDOM.render(<App title="Shakespeare Insults" />, document.getElementById('root'));
